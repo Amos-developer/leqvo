c09551b2efbce992b4cf000705a695f789d09967
Amos-developer <108217876+Amos-developer@users.noreply.github.com>
2026-07-30 01:45:54 +0300

    Removing the preloader globally

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,43 +1,15 @@
 <template>
-  <AppPreloader v-if="isLoading" />
-  <template v-else>
-    <div :class="{ 'admin-route-shell': route.name === 'admin' }">
+  <div :class="{ 'admin-route-shell': route.name === 'admin' }">
     <RouterView />
     <AppFooter v-if="showFooter" />
-    </div>
-  </template>
+  </div>
 </template>
 
 <script setup>
-import { computed, onMounted, ref, watch } from "vue";
+import { computed } from "vue";
 import { useRoute } from "vue-router";
 import AppFooter from "./components/AppFooter.vue";
-import AppPreloader from "./components/AppPreloader.vue";
 
 const route = useRoute();
-const isLoading = ref(true);
-const hasMounted = ref(false);
 const showFooter = computed(() => route.meta.requiresAuth && !route.meta.hideFooter);
-const PRELOADER_DURATION_MS = 2000;
-
-onMounted(() => {
-  setTimeout(() => {
-    isLoading.value = false;
-    hasMounted.value = true;
-  }, PRELOADER_DURATION_MS);
-});
-
-watch(
-  () => route.fullPath,
-  () => {
-    if (!hasMounted.value) {
-      return;
-    }
-
-    isLoading.value = true;
-    setTimeout(() => {
-      isLoading.value = false;
-    }, PRELOADER_DURATION_MS);
-  }
-);
 </script>
