3d8f2d8d9145c61a7bd7d6a8ca84a5802b5a5213
Amos-developer <108217876+Amos-developer@users.noreply.github.com>
2026-08-04 10:57:02 +0300

    move vue ref import into script setup

--- frontend/src/views/DailySpinView.vue
+++ frontend/src/views/DailySpinView.vue
@@ -1,5 +1,5 @@
-import { computed, onMounted, ref } from "vue";
 <script setup>
+import { computed, onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 import { getDailySpinStatus, runDailySpin } from "../utils/api";
 
